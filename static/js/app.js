1d27b73961a9cbc65a498674415ae3ee9eae46bb
adamholcomb <adam.holcomb@ymail.com>
2021-07-30 14:41:43 -0500

    Finish building base chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,71 +1,61 @@
-// Read in data
 
-function BarCharts(ID) {
-
-    d3.json("data/samples.json").then(data => {
+// Create charts function
+function Charts(ID) {
+    // Read in data
+    d3.json("Data/samples.json").then((data)=> {
         console.log(data)
+     
+        // Filter down to ID data 
+        var samp = data.samples.filter(sampleName => sampleName.id.toString() == ID)[0]
+        console.log(samp)
 
-        // var samples = Object.values(data.samples)
-        // console.log(samples)
 
         // Get sample values
-        var samplevalues = data.samples.map(samples => samples.sample_values)
-        // console.log(samplevalues)
-
-        // Make sure data is sorted in descending order
-        let samplesort = samplevalues.sort((a,b)=>b-a)
-        // console.log(samplesort)
-
-        // Slice down to top 10 samples for each person
-        var sampletop10 = samplesort.map(sampleslice=>sampleslice.slice(0,10))
-        console.log(sampletop10[0]);
-
-        
-        // Get IDs
-        var otu_id = data.samples.map(ids => ids.otu_ids)
-        // console.log(otu_id)
-
-        // Slice down to top 10 ids
-        var otutop10 = otu_id.map(idslice => idslice.slice(0,10))
-        console.log(otutop10[0])
-
-        var otuidtop10 = otutop10[0].map(otuslice => `OTU ${otuslice}`)
-        console.log(otuidtop10)
-
+        var samplevalues = samp.sample_values.slice(0, 10).reverse()
+        console.log(samplevalues)
+  
+        // Get IDs 
+        var otutop10 = samp.otu_ids.slice(0, 10)
+        var otuids = (otutop10.map(otu => `OTU ${otu}`)).reverse()
+        console.log(otuids)
 
         // Get labels
-        var otu_label = data.samples.map(labels => labels.otu_labels)
-        // console.log(otu_label)
-
-        // Slice down to top 10 lables
-        var labeltop10 = otu_label.map(labelslice => labelslice.slice(0,10))
-        console.log(labeltop10)
-
-        let filteredSample = data.samples.filter(sampleName => sampleName.id == ID)[0]
-        console.log(filteredSample)
-
-        let otu_ids = filteredSample.otu_ids.slice(0, 10)
-        console.log(otu_ids)
-
-        let yticksBar = otu_ids.map(otuID => `OTU ${otuID}`)
-        console.log(yticksBar)
+        var otulabels = samp.otu_labels.slice(0, 10);
+        console.log(otulabels)
 
 
-        // Create trace
-        function init() {
-            data1 = [{
-                X: sampletop10[0],
-                // x: [1,2,3],
-                y: otuidtop10,
-                // y: [3,2,1],
-                text: labeltop10[0],
-                type: 'bar',
-                orientation: 'h'
-            }]
-            Plotly.newPlot("bar", data1)
-        } 
-        init()
+        // Create bar graph
+        var bar = [{
+            x: samplevalues,
+            y: otuids,
+            text: otulabels,
+            type:"bar",
+            orientation: "h",
+        }];
+        Plotly.newPlot("bar", bar);
 
-    })
-}
-BarCharts(940)
+
+        
+      });
+  }  
+Charts(940);
+
+
+
+
+        
+        //--------------------------------------------------------
+        // Get sample values
+        // var samplevalues = samples.map(samples => samples.sample_values)
+        // let samplesort = samplevalues.sort((a,b)=>b-a)
+        // var sampletop10 = samplesort.map(sampleslice=>sampleslice.slice(0,10))
+        
+        // Get IDs
+        // var otu_id = data.samples.map(ids => ids.otu_ids)
+        // var otutop10 = otu_id.map(idslice => idslice.slice(0,10))
+        // var otuidtop10 = otutop10[0].map(otuslice => `OTU ${otuslice}`)
+
+        // Get labels
+        // var otu_label = data.samples.map(labels => labels.otu_labels)
+        // var labeltop10 = otu_label.map(labelslice => labelslice.slice(0,10))
+        //----------------------------------------------------------
